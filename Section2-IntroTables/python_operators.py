ff3c808a36b526a92172710a5dd2970eeda3f3db
neo-mahe <maheshb.neo@gmail.com>
2020-09-08 17:05:33 +0530

    adding 1st section of the Udemy course

---
+++ Section2-IntroTables/python_operators.py
@@ -0,0 +1,26 @@
+print(10*2)
+
+# To print the power of a number
+str1 = 10**2
+str2 = 10**3
+print("10 to the power of 2 is "+str(str1))
+print("10 to the power of 3 is "+str(str2))
+
+print(9/2)
+
+print(9%2)
+
+test = pow(2, 3)
+print(test)
+
+max_number = max(98, 67, 85, 54)
+print(max_number)
+
+min_number = min(98, 67, 85, 54)
+print(min_number)
+
+
+
+# naming convention for variables
+count12 = 12
+c_ount = 12
